c5152a700170aa89221731975b4bce6530443963
Enanyy <742216502@qq.com>
2019-12-05 17:30:47 +0800

    UI

--- Assets/Scripts/UnityEngine/UI/Tab.cs
+++ Assets/Scripts/UnityEngine/UI/Tab.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.Events;
 
 
@@ -8,84 +6,69 @@ namespace UnityEngine.UI
 {
     public class Tab : MonoBehaviour
     {
-        Dictionary<Toggle, bool> mToggles = new Dictionary<Toggle, bool>();
-        List<Toggle> mTogglesChanged = new List<Toggle>();
-        // Start is called before the first frame update
-
+       
         ToggleGroup mGroup;
 
         public TabEvent onTabValueChanged = new TabEvent();
-        public TabEvent onTabAdd = new TabEvent();
-        private bool mChildrenChanged = true;
+
+        private bool mInited = false;
         void Start()
         {
             mGroup = GetComponent<ToggleGroup>();
+
             if (mGroup == null) mGroup = gameObject.AddComponent<ToggleGroup>();
 
-            InitTab();
 
+            RegisterToggle();
+
+            mInited = true;
         }
 
-        private void InitTab()
+        private void RegisterToggle()
         {
             for (int i = 0; i < transform.childCount; ++i)
             {
                 var toggle = transform.GetChild(i).GetComponent<Toggle>();
                 if (toggle != null && toggle.enabled && toggle.gameObject.activeInHierarchy)
                 {
-                    if (toggle.group != mGroup)
-                    {
-                        mGroup.RegisterToggle(toggle);
-                    }
-
-                    toggle.onValueChanged.RemoveListener(OnValueChanged);
-                    toggle.onValueChanged.AddListener(OnValueChanged);
-                    if (mToggles.ContainsKey(toggle) == false)
-                    {
-                        mToggles.Add(toggle, toggle.isOn);
-                        onTabAdd.Invoke(toggle);
-                    }
-                    else
-                    {
-
-                    }
+                    RegisterToggle(toggle);
                 }
             }
-
-            OnValueChanged(true);
-
-            mChildrenChanged = false;
         }
-        void OnValueChanged(bool value)
+
+        public void RegisterToggle(Toggle toggle)
         {
-            mTogglesChanged.Clear();
-            var it = mToggles.GetEnumerator();
-            while (it.MoveNext())
+            if(toggle== null)
             {
-                var toggle = it.Current.Key;
-                if (toggle && toggle.enabled && toggle.gameObject.activeInHierarchy)
-                {
-                    if (it.Current.Value != toggle.isOn || mChildrenChanged)
-                    {
-                        mTogglesChanged.Add(toggle);
-                    }
-                }
+                return;
+            }
+            
+            if (toggle.group != mGroup)
+            {
+                mGroup.RegisterToggle(toggle);
             }
 
-            for (int i = 0; i < mTogglesChanged.Count; ++i)
+            toggle.onValueChanged.RemoveAllListeners();
+            toggle.onValueChanged.AddListener((value) => {
+
+                onTabValueChanged.Invoke(toggle);
+            });
+
+            if(mInited == false)
             {
-                var toggle = mTogglesChanged[i];
-                mToggles[toggle] = toggle.isOn;
                 onTabValueChanged.Invoke(toggle);
             }
         }
+       
 
         private void OnTransformChildrenChanged()
         {
-            mChildrenChanged = true;
             Debug.Log("OnTransformChildrenChanged");
-            InitTab();
+            mInited = false;
 
+            RegisterToggle();
+
+            mInited = true;
         }
 
         public class TabEvent : UnityEvent<Toggle>
